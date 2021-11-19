3af3654b9e15952ac53bb42e932495fc099fc100
ssonggary <84754325+ssonggary@users.noreply.github.com>
2021-11-19 20:55:07 +0900

    2021.11.19

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+
+//swipr1
+new Swiper('.swiper1', { 
+    autoplay: {delay: 5000},
+    loop: true
+});
+
+//swipr2
+new Swiper('.swiper2', {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    loop: true
+});
+
+//swipr3
+new Swiper('.swiper3', {
+    loop: true
+});
